b35b4edabb062a3cb5d3e32609d9103f4f523721
Phelix <phelixmbani@gmail.com>
2025-04-01 17:15:47 +0300

    configure docker for redis implementation

--- src/handlers/services/task_service.py
+++ src/handlers/services/task_service.py
@@ -1,4 +1,6 @@
 from src.handlers.repositories.task_repository import  TaskRepository
+from src.services_layer.utilities.redis_client import cache_data, get_cached_data, invalidate_cache
+import json 
 
 class TaskService:
     @staticmethod
@@ -9,11 +11,25 @@ class TaskService:
             raise e
     
     @staticmethod
+    def get_task_id(user_id: int):
+        cache_key = f"user_{user_id}:tasks"
+        cache_tasks = get_cached_data(cache_key)
+        if cache_tasks:
+            return json.loads(cache_tasks.decode('utf-8'))
+        
+        tasks = TaskRepository.get_task_by_id(user_id)
+        cache_data(cache_key, json.dumps([task.to_dict() for task in tasks]))
+        return tasks
+    
+    @staticmethod
     def create_task(data):
         if not data.get('title'):
             raise ValueError ('Title is required')
         try:
-            return TaskRepository.create_task(data)
+        
+            task = TaskRepository.create_task(data)
+            invalidate_cache(f"user_{data['user_id']}: tasks")
+            return task
         except Exception as e:
             raise e
         
--- src/services_layer/auth/auth.py
+++ src/services_layer/auth/auth.py
@@ -1,6 +1,7 @@
 import jwt
 from datetime import datetime, timedelta
 from src.config.config import Config
+from src.services_layer.utilities.redis_client import cache_data, get_cached_data
 
 class AuthService:
     @staticmethod
@@ -10,13 +11,19 @@ class AuthService:
             "role": role,
             "exp": datetime.utcnow() + timedelta(hours=1)
         }
-        return jwt.encode(payload, Config.SECRET_KEY, algorithm="HS256")
+        token = jwt.encode(payload, Config.SECRET_KEY, algorithm="HS256")
+        cache_data(f"token_{user_id}", role)
+        return token
     
     @staticmethod
     def verify_token(token):
         try:
             payload = jwt.decode(token, Config.SECRET_KEY, algorithms="HS256")
-            return payload["user_id"]
+            cache_role = get_cached_data(f"token_{payload['user_id']}")
+            
+            if cache_role:
+                return payload["user_id"], cache_role.decode('utf-8')
+            return payload["user_id"], payload['role']
         except jwt.ExpiredSignatureError:
             raise ValueError ("Token has expired")
         except jwt.InvalidTokenError:
